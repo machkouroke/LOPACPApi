9dbbe21b492a570594c0a8a41b5792c29ddcdf10
MorelK1 <kouhossounon.vianney@gmail.com>
2023-01-07 19:45:55 +0100

    `feat` add requirements file

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
 from flask import Flask
+from flask_cors import CORS
+import shelve
 
-app = Flask(__name__)
+model = shelve.open('model.h5')
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def create_app():
+    app = Flask(__name__)
+    CORS(app, resources={r"/*": {"origins": "*"}})
+
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
+        response.headers.add('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE,OPTIONS')
+        return response
+
+    def upload():
+        pass
+
+    @app.route('/')
+    def hello_world():  # put application's code here
+        return 'Hello World!'
+
+    return app
 
 
 if __name__ == '__main__':
+    app = create_app()
     app.run()
