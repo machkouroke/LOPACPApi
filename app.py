8204ec23db3155cc2bd753a0de20e93dd89cfc9f
machkouroke <machkour20.mo1@gmail.com>
2023-01-08 10:29:18 +0000

    `feat` Add of error management and file upload correction

--- app.py
+++ app.py
@@ -1,51 +1,69 @@
 import os
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort
+from error import setup_error_template
 from flask_cors import CORS
 import numpy as np
 import cv2
 import shelve
 
-model = shelve.open('model.h5')
-
 
 def create_app():
     app = Flask(__name__)
     CORS(app, resources={r"/*": {"origins": "*"}})
+    setup_error_template(app)
 
     @app.after_request
     def after_request(response):
-        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
-        response.headers.add('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE,OPTIONS')
+        response.headers.add('Access-Control-Allow-Headers',
+                             'Content-Type,Authorization,true')
+        response.headers.add('Access-Control-Allow-Methods',
+                             'GET,PATCH,POST,DELETE,OPTIONS')
         return response
 
     def upload():
-        img = request.files["img"]
-        img_path = "static/images/" + img.filename
-        img.save(img_path)
-        return "static\\images\\" + img.filename, img_path
+        file = request.files['img']
+
+        if file:
+            if not os.path.exists('uploads'):
+                os.makedirs('uploads')
+            file.save(os.path.join('uploads', file.filename))
+            img_path = "uploads/" + img.filename
+            return img_path
+        else:
+            raise ValueError('No file uploaded')
 
     def predict(img_path):
         img = cv2.imread(img_path)
-        result = model.predict(img)
+        with shelve.open("variables") as variables:
+            model = variables["model"]
+            result = model.predict(img)
         return np.argmax(result)
 
     @app.route('/prediction', methods=['POST'])
     def get_prediction():
-        path, img_path = upload()
-        result = predict(path)
+        try:
+            img_path = upload()
+            result = predict(path)
+            return jsonify({
+                'success': True,
+                'result': result
+            })
+        except ValueError as e:
+            abort(400, e)
+        except Exception as e:
+            abort(500, e)
+
+    @app.route('/', methods=['Get', 'POST'])
+    def hello_world():  # put application's code here
         return jsonify({
             'success': True,
-            'result': result
+            'result': "Hello World 1.0"
         })
-
-    @app.route('/')
-    def hello_world():  # put application's code here
-        return 'Hello World!'
 
     return app
 
 
 if __name__ == '__main__':
     app = create_app()
-    app.run()
+    app.run(debug=True)
